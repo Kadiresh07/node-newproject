13a84421159c50384bd31ca0d0515c2adf4b9543
Kadiresh07 <kadiresh06@gmail.com>
2025-11-17 17:21:30 +0530

    added dev

--- config/prod.js
+++ config/prod.js
@@ -5,5 +5,5 @@ module.exports ={
     googleClientID :process.env.GOOGLE_CLIENT_ID,
     googleClientSecret :process.env.GOOGLE_CLIENT_SECRET,
     mongoUrl:process.env.MONGO_URI,
-    cookieKey:process.env.GOOGLE_CLIENT_
+    cookieKey:process.env.COOKIE_KEY
 };
